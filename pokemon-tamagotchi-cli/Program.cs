7254e7e23239b1e39d44425918b80e6180871938
leoLUIGY <luigyleonidas@gmail.com>
2025-06-13 10:16:35 -0300

    get request in pokemon api and show in console

--- pokemon-tamagotchi-cli/Program.cs
+++ pokemon-tamagotchi-cli/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using RestSharp;
+
+var client  =  new RestClient("https://pokeapi.co/api/v2/pokemon/");
+var request = new RestRequest("", Method.Get);
+
+var response = client.Execute(request);
+
+if (response.StatusCode == System.Net.HttpStatusCode.OK)
+{
+    Console.WriteLine(response.Content);
+    
+}
+else 
+{
+    Console.WriteLine($"Error: {response.StatusCode} - {response.ErrorMessage}");
+}
+
+
